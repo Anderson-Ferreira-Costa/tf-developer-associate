02c5aa5b92fa9d55e1fd44368675230fc722ec8a
Anderson Ferreira <andersonferreiracosta@outlook.com>
2022-11-01 02:00:24 -0300

    ok

--- codepipeline/main.tf
+++ codepipeline/main.tf
@@ -3,10 +3,10 @@ terraform {
     bucket  = "anderson-ferreira"
     key     = "terraform-state/codepipeline/terraform.tfstate"
     region  = "us-east-1"
-    profile = "anderson"
+#    profile = "anderson"
   }
 }
 provider "aws" {
   region  = "us-east-1"
-  profile = "anderson"
+#  profile = "anderson"
 }
